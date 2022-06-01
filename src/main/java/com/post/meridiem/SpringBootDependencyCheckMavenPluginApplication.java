df624cabf3844be479969e2c439d8513874fc26a
linosebastian <talk2linojoy@gmail.com>
2022-06-01 10:52:42 +0530

    Spring boot dependency check maven plugin initial upload

---
+++ src/main/java/com/post/meridiem/SpringBootDependencyCheckMavenPluginApplication.java
@@ -0,0 +1,13 @@
+package com.post.meridiem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDependencyCheckMavenPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDependencyCheckMavenPluginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/post/meridiem/SpringBootDependencyCheckMavenPluginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.post.meridiem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDependencyCheckMavenPluginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
